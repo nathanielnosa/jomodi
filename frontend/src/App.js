7306f7779d17db5e7e9f808f6aa1a1d76bf3dea7
Parkson Tano Daniel <tanoparksonsilencer@gmail.com>
2023-07-16 07:55:26 +0100

    all pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,39 +4,18 @@ import NewsLetter from './components/NewsLetter';
 import Home from './pages/Home';
 import Store from './pages/Store';
 import Checkout from './pages/Checkout';
-import Product from './pages/Product';
 
 function App() {
   return (
     <div>
       <Header />
-      <Product />
       {/* <Store /> */}
       {/* <Checkout /> */}
-      {/* <Home />   */}
+      <Home />  
       <NewsLetter />
       <Footer />
     </div>
   );
 }
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// Add the script references
-const scriptSources = [
-  'js/jquery.min.js',
-  'js/bootstrap.min.js',
-  'js/slick.min.js',
-  'js/nouislider.min.js',
-  'js/jquery.zoom.min.js',
-  'js/main.js',
-];
-
-scriptSources.forEach((src) => {
-  const script = document.createElement('script');
-  script.src = src;
-  script.async = true;
-  document.body.appendChild(script);
-});
 
 export default App;
