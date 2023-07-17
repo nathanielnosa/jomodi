d78728309c0712f127668c2e42d02ea6a860d986
Parkson Tano Daniel <tanoparksonsilencer@gmail.com>
2023-07-17 13:11:59 +0100

    filter brand and category

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -4,21 +4,41 @@ import { API_URL } from '../constants';
 import { Link } from 'react-router-dom';
 import Slider from '@mui/material/Slider';
 
-function Sidebar({max, min}) {
+function Sidebar({ max, min, selectedCategories, selectedBrands, onCategoryChange, onBrandChange }) {
 	const [categories, setCategories] = useState([]);
 	const [brands, setBrands] = useState([]);
 	const [products, setProducts] = useState([]);
 	const [maxPrice, setMaxPrice] = useState(max);
 	const [minPrice, setMinPrice] = useState(min);
 
+	const handleCategoryChange = (categoryId) => {
+		const isSelected = selectedCategories.includes(categoryId);
+
+		if (isSelected) {
+			onCategoryChange(selectedCategories.filter((id) => id !== categoryId));
+		} else {
+			onCategoryChange([...selectedCategories, categoryId]);
+		}
+	};
+
+	const handleBrandChange = (brandId) => {
+		const isSelected = selectedBrands.includes(brandId);
+
+		if (isSelected) {
+			onBrandChange(selectedBrands.filter((id) => id !== brandId));
+		} else {
+			onBrandChange([...selectedBrands, brandId]);
+		}
+	};
 
 	useEffect(() => {
-		axios.get(`${API_URL}product/product_detail/`)
-			.then(res => {
+		axios
+			.get(`${API_URL}product/product_detail/`)
+			.then((res) => {
 				console.log(res.data);
 				setProducts(res.data.results);
 			})
-			.catch(err => {
+			.catch((err) => {
 				console.log(err);
 			});
 	}, []);
@@ -29,7 +49,7 @@ function Sidebar({max, min}) {
 
 	const addMinPrice = () => {
 		setMinPrice(minPrice + 1000);
-	}
+	};
 
 	const subtractMaxPrice = () => {
 		setMaxPrice(maxPrice - 1000);
@@ -40,45 +60,49 @@ function Sidebar({max, min}) {
 	};
 
 	useEffect(() => {
-		axios.get(API_URL + 'category/category/')
-			.then(res => {
+		axios
+			.get(API_URL + 'category/category/')
+			.then((res) => {
 				console.log(res.data.results[0].id);
 				setCategories(res.data.results);
 			})
-			.catch(err => {
+			.catch((err) => {
 				console.log(err);
 			});
 	}, []);
 
 	useEffect(() => {
-		axios.get(API_URL + 'category/brand/')
-			.then(res => {
+		axios
+			.get(API_URL + 'category/brand/')
+			.then((res) => {
 				console.log(res.data.results[0].id);
 				setBrands(res.data.results);
 			})
-			.catch(err => {
+			.catch((err) => {
 				console.log(err);
 			});
 	}, []);
+
 	return (
 		<div id="aside" className="col-md-3">
-
 			<div className="aside">
 				<h3 className="aside-title">Categories</h3>
 				<div className="checkbox-filter">
-
-					{
-						categories?.map((category, index) => (
-							<div className="input-checkbox" key={category.id}>
-								<input type="checkbox" id={`category-${category.id}`} />
-								<label htmlFor={`category-${category.id}`}>
-									<span></span>
-									{category.name}
-									<small>(120)</small>
-								</label>
-							</div>
-						))
-					}
+					{categories?.map((category, index) => (
+						<div className="input-checkbox" key={category.id}>
+							<input
+								type="checkbox"
+								id={`category-${category.id}`}
+								checked={selectedCategories.includes(category.id)}
+								onChange={() => handleCategoryChange(category.id)}
+							/>
+							<label htmlFor={`category-${category.id}`}>
+								<span></span>
+								{category.name}
+								<small>(120)</small>
+							</label>
+						</div>
+					))}
 				</div>
 			</div>
 
@@ -96,7 +120,7 @@ function Sidebar({max, min}) {
 							max={max}
 							step={1}
 							orientation="horizontal"
-							sx={{ width: "100%", color: "#ff0000" }}
+							sx={{ width: '100%', color: '#ff0000' }}
 						/>
 					</div>
 					<div className="input-number price-min">
@@ -129,60 +153,50 @@ function Sidebar({max, min}) {
 						</span>
 					</div>
 				</div>
-
-
 			</div>
 
 			<div className="aside">
 				<h3 className="aside-title">Brand</h3>
 				<div className="checkbox-filter">
-					{
-						brands?.map((brand, index) => (
-							<div className="input-checkbox">
-								<input type="checkbox" id={`brand-${brand.id}`} />
-								<label htmlFor={`brand-${brand.id}`}>
-									<span></span>
-									{brand.name}
-									<small>(578)</small>
-								</label>
-							</div>
-						))
-					}
+					{brands?.map((brand, index) => (
+						<div className="input-checkbox" key={brand.id}>
+							<input
+								type="checkbox"
+								id={`brand-${brand.id}`}
+								checked={selectedBrands.includes(brand.id)}
+								onChange={() => handleBrandChange(brand.id)}
+							/>
+							<label htmlFor={`brand-${brand.id}`}>
+								<span></span>
+								{brand.name}
+								<small>(578)</small>
+							</label>
+						</div>
+					))}
 				</div>
 			</div>
 
 			<div className="aside">
 				<h3 className="aside-title">Top selling</h3>
-					{
-						products?.map((product, index) => (
-							<div key={index} className="product-widget">
-								<div className="product-img">
-									<img src={product?.image} alt="" />
-								</div>
-								<div className="product-body">
-									<p className="product-category">
-										{
-											product?.category?.name
-										}
-									</p>
-									<h3 className="product-name">
-										<Link to={`/product/${product?.id}`}>
-										{
-											product?.name
-										}
-										</Link></h3>
-									<h4 className="product-price">${product?.price} <del className="product-old-price">${product?.cancel_price}</del></h4>
-								</div>
-							</div>
-						))
-					}
-
-
+				{products?.map((product, index) => (
+					<div key={index} className="product-widget">
+						<div className="product-img">
+							<img src={product?.image} alt="" />
+						</div>
+						<div className="product-body">
+							<p className="product-category">{product?.category?.name}</p>
+							<h3 className="product-name">
+								<Link to={`/product/${product?.id}`}>{product?.name}</Link>
+							</h3>
+							<h4 className="product-price">
+								${product?.price} <del className="product-old-price">${product?.cancel_price}</del>
+							</h4>
+						</div>
+					</div>
+				))}
 			</div>
-
 		</div>
-
-	)
+	);
 }
 
-export default Sidebar
+export default Sidebar;
--- frontend/src/pages/Store.js
+++ frontend/src/pages/Store.js
@@ -6,9 +6,11 @@ import axios from 'axios'
 import { API_URL } from '../constants'
 
 function Store() {
-  const [products, setProducts] = useState([])
+  const [products, setProducts] = useState([]);
   const [maxPrice, setMaxPrice] = useState(0);
   const [minPrice, setMinPrice] = useState(0);
+  const [selectedCategories, setSelectedCategories] = useState([]);
+  const [selectedBrands, setSelectedBrands] = useState([]);
 
   useEffect(() => {
     axios
@@ -31,45 +33,63 @@ function Store() {
       });
   }, []);
 
+  const filteredProducts = products.filter((product) => {
+    const categoryMatch = selectedCategories.some((id) => id === product?.category?.id);
+    const brandMatch = selectedBrands.some((id) => id === product?.brand?.id);
+    return categoryMatch || brandMatch;
+  });
+
+
+  console.log(selectedCategories)
+  console.log(selectedBrands)
+
   return (
     <div className="section">
       <div className="container">
         <div className="row">
-          <Sidebar max={maxPrice} min={minPrice} />
+          <Sidebar
+            max={maxPrice}
+            min={minPrice}
+            selectedCategories={selectedCategories}
+            selectedBrands={selectedBrands}
+            onCategoryChange={setSelectedCategories}
+            onBrandChange={setSelectedBrands}
+          />
 
           <div id="store" className="col-md-9">
-
             <StoreTop />
 
             <div className="row">
-              {
-                products.map((product, index) => {
-                  return (
-                    <ProductCard product={product} key={index} />
-                  )
-                })
-              }
-         
+              {filteredProducts.map((product, index) => (
+                <ProductCard product={product} key={index} />
+              ))}
             </div>
+
             <div className="store-filter clearfix">
-              <span className="store-qty">Showing {products?.length} products</span>
+              <span className="store-qty">Showing {filteredProducts.length} products</span>
               <ul className="store-pagination">
                 <li className="active">1</li>
-                <li><a href="#">2</a></li>
-                <li><a href="#">3</a></li>
-                <li><a href="#">4</a></li>
-                <li><a href="#"><i className="fa fa-angle-right"></i></a></li>
+                <li>
+                  <a href="#">2</a>
+                </li>
+                <li>
+                  <a href="#">3</a>
+                </li>
+                <li>
+                  <a href="#">4</a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i className="fa fa-angle-right"></i>
+                  </a>
+                </li>
               </ul>
             </div>
-
           </div>
-
         </div>
-
       </div>
-
     </div>
-  )
+  );
 }
 
-export default Store
+export default Store;
