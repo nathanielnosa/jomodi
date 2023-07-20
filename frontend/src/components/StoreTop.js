9171f0e1fe1f31b4106d04a7fcce2977ada30691
Parkson Tano Daniel <tanoparksonsilencer@gmail.com>
2023-07-20 03:36:15 +0100

    fix sort

--- frontend/src/components/StoreTop.js
+++ frontend/src/components/StoreTop.js
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-function StoreTop() {
+function StoreTop({ onSortChange }) {
+    // State to keep track of the selected sorting option
+    const [selectedSortOption, setSelectedSortOption] = useState('1');
+
+    // Function to handle the change in sorting option
+    const handleSortChange = (event) => {
+        const selectedValue = event.target.value;
+        setSelectedSortOption(selectedValue);
+        onSortChange(selectedValue); // Call the onSortChange prop with the selected value
+    };
+
     return (
         <div className="store-filter clearfix">
             <div className="store-sort">
                 <label>
                     Sort By:
-                    <select className="input-select">
+                    <select className="input-select" value={selectedSortOption} onChange={handleSortChange}>
                         <option value="1">Recommended</option>
                         <option value="2">Price: Low to High</option>
                         <option value="3">Price: High to Low</option>
@@ -15,7 +25,7 @@ function StoreTop() {
                 </label>
             </div>
         </div>
-    )
+    );
 }
 
-export default StoreTop
+export default StoreTop;
