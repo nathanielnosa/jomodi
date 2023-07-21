551358acd09a907f89f7d7702d17eb9f8deb3e35
Parkson Tano Daniel <tanoparksonsilencer@gmail.com>
2023-07-21 18:30:50 +0100

    stat

--- frontend/src/pages/OrderReturn.js
+++ frontend/src/pages/OrderReturn.js
@@ -1,9 +1,63 @@
 import React from 'react'
+import { Container, Text } from '@mantine/core';
+
 
 function OrderReturn() {
-  return (
-    <div>OrderReturn</div>
-  )
+    return (
+        <Container size="lg">
+            <Text size="xl" weight={700} align="center" style={{ marginBottom: '1rem' }}>
+                Orders and Returns Summary for Jomodi
+            </Text>
+            <Text align="justify" style={{ marginBottom: '0.5rem' }}>
+                Order Placement:
+                Thank you for shopping at Jomodi! Placing an order with us is simple and convenient. Browse our extensive selection of products, add your desired items to the cart, and proceed to checkout. You will need to provide your shipping address, contact information, and payment details to complete the order. Once your order is successfully placed, you will receive an order confirmation email with the details of your purchase.
+
+            </Text>
+            <Text align="justify" style={{ marginBottom: '0.5rem' }}>
+                Order Processing and Shipment:
+                We strive to process and ship your order as swiftly as possible. Orders typically undergo processing within 2 business days, and shipping times may vary depending on your location and chosen shipping method. Once your order is shipped, you will receive a shipping confirmation email with tracking details, allowing you to monitor the status of your delivery.
+
+            </Text>
+            <Text align="justify" style={{ marginBottom: '0.5rem' }}>
+                Shipping Options and Rates:
+                We offer various shipping options to suit your needs, including standard, express, and international shipping. Shipping rates are calculated based on the shipping method, destination, and the weight and size of the items in your order. Please review the available shipping options and rates during the checkout process.
+
+            </Text>
+            <Text align="justify" style={{ marginBottom: '0.5rem' }}>
+                Order Tracking:
+                To track your order, simply access the tracking information provided in the shipping confirmation email. You can also log in to your account on our website to view your order history and track the status of your current orders.
+
+            </Text>
+            <Text align="justify" style={{ marginBottom: '0.5rem' }}>
+                Returns and Refunds:
+                We want you to be completely satisfied with your purchase. If, for any reason, you are not pleased with your order, you can initiate a return within 7 days of receiving the product. Please refer to our full Return Policy for specific details on eligible products and return process.
+
+            </Text>
+            <Text align="justify" style={{ marginBottom: '0.5rem' }}>
+                How to Initiate a Return:
+                To initiate a return, log in to your account and visit the "Order History" section. Locate the specific order containing the item you wish to return and click on "Initiate Return." Follow the step-by-step instructions to complete the return request. Once your return request is approved, you will receive further instructions on how to ship the item back to us.
+
+            </Text>
+            <Text align="justify" style={{ marginBottom: '0.5rem' }}>
+                Return Eligibility:
+                Returned items must be in their original condition, unused, and with all original packaging and tags intact. Certain items, such as personalized or perishable products, may not be eligible for return due to their nature. Please carefully review our Return Policy for detailed information on eligible products.
+
+            </Text>
+            <Text align="justify" style={{ marginBottom: '0.5rem' }}>
+                Refunds:
+                Once we receive and inspect the returned item, we will process your refund. Refunds are typically issued to the original payment method within 2 to 3 business days after the return is approved. Please note that the time it takes for the refund to reflect in your account may vary depending on your payment provider.
+
+            </Text>
+            <Text align="justify" style={{ marginBottom: '0.5rem' }}>
+                Contact Us:
+                If you have any questions, concerns, or need assistance with your order or return, our customer support team is here to help. You can reach us via contact@jomodi.com.
+
+            </Text>
+            <Text align="justify" style={{ marginBottom: '0.5rem' }}>
+                Thank you for choosing Jomodi for your shopping needs. We value your satisfaction and are committed to providing you with a seamless shopping experience, from order placement to returns and beyond. Happy shopping!
+            </Text>
+        </Container>
+    )
 }
 
 export default OrderReturn
--- frontend/src/pages/PrivacyPolicy.js
+++ frontend/src/pages/PrivacyPolicy.js
@@ -1,8 +1,81 @@
 import React from 'react'
+import { Container, Text } from '@mantine/core'
 
 function PrivacyPolicy() {
   return (
-    <div>PrivacyPolicy</div>
+    <Container size="lg">
+        <Text size="lg" weight={700} style={{ marginBottom: '1rem' }}>
+              Privacy Policy Summary for Jomodi
+        </Text>
+        <Text align='justify' style={{ marginBottom: '0.5rem' }}>
+              At Jomodi, we are committed to protecting your privacy and ensuring the security of your personal information. This Privacy Policy outlines how we collect, use, disclose, and safeguard your data when you visit and use our website or engage in transactions with us. By accessing and using our services, you agree to the practices described in this summary and the full Privacy Policy.
+        </Text>
+
+          <Text align='justify' style={{ marginBottom: '0.5rem' }}>
+              Information Collection:
+              We collect certain personal information when you create an account, make a purchase, or interact with our website. This may include your name, contact details, shipping address, payment information, and browsing activity.
+
+          </Text>
+
+
+          <Text align='justify' style={{ marginBottom: '0.5rem' }}>
+              How We Use Your Information:
+              The information we collect is primarily used to process your orders, provide customer support, and improve your shopping experience. We may also use your data to personalize content, send you promotional offers, and conduct market research.
+
+          </Text>
+          <Text align='justify' style={{ marginBottom: '0.5rem' }}>
+              Data Sharing:
+              We do not sell, rent, or trade your personal information to third parties for their marketing purposes. However, we may share your data with trusted service providers who assist us in fulfilling orders, processing payments, and delivering products.
+
+          </Text>
+
+
+          <Text align='justify' style={{ marginBottom: '0.5rem' }}>
+              Cookies and Similar Technologies:
+              We use cookies and similar technologies to enhance your browsing experience, analyze website traffic, and tailor advertisements. You have the option to manage your cookie preferences through your browser settings.
+
+          </Text>
+
+
+          <Text align='justify' style={{ marginBottom: '0.5rem' }}>
+              Data Security:
+              We prioritize the security of your information and implement industry-standard measures to protect it from unauthorized access, disclosure, or alteration. However, please understand that no method of data transmission over the internet or electronic storage is entirely secure.
+
+          </Text>
+
+
+          <Text align='justify' style={{ marginBottom: '0.5rem' }}>
+              Children's Privacy:
+              Our services are not intended for children under the age of 13. We do not knowingly collect personal information from children, and if we become aware of such data, we will take immediate steps to delete it.
+
+          </Text>
+
+
+          <Text align='justify' style={{ marginBottom: '0.5rem' }}>
+              Updates to Privacy Policy:
+              We may periodically update this Privacy Policy to reflect changes in our practices or applicable laws. We encourage you to review the policy regularly to stay informed about how we handle your information.
+
+          </Text>
+
+          <Text align='justify' style={{ marginBottom: '0.5rem' }}>
+              Your Privacy Choices:
+              You have the right to access, correct, and delete your personal information held by us. You can also opt-out of receiving marketing communications from us. For more details on your privacy choices, please refer to the full Privacy Policy.
+
+          </Text>
+          <Text align='justify' style={{ marginBottom: '0.5rem' }}>
+              Contact Us:
+              If you have any questions, concerns, or requests related to your privacy or this summary, please contact our Privacy Officer using the information provided in the full Privacy Policy.
+
+          </Text>
+          <Text align='justify' style={{ marginBottom: '0.5rem' }}>
+              By using our website and services, you consent to the terms of this Privacy Policy. Thank you for entrusting Jomodi with your shopping needs, and we assure you that your privacy is our top priority.
+
+          </Text>
+          
+
+
+
+    </Container>
   )
 }
 
