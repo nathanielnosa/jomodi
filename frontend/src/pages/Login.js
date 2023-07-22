1f159a4bd103652d95dd6bffa943d0f29276bbce
Parkson Tano Daniel <tanoparksonsilencer@gmail.com>
2023-07-22 20:01:41 +0100

    fix login

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Button, Card, TextInput, Text } from '@mantine/core'
+import { Button, Card, TextInput, Text, Group, Container, Loader } from '@mantine/core'
 import { useForm, isNotEmpty, isEmail, isInRange, hasLength, matches } from '@mantine/form';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
@@ -12,152 +12,135 @@ export default function Login({ handleLogin }) {
     const [error, setError] = useState('');
     const [phone, setPhone] = useState('');
     const [code, setCode] = useState('');
+    const [showError, setShowError] = useState(false);
+    const [submitting, setSubmitting] = useState(false);
     const { login } = useAuth();
     const navigate = useNavigate();
 
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        setSubmitting(true);
         // Send the form data to Django backend
-        axios.post(`${API_URL}auth/send-sms/` , { numbers: phone })
+        axios.post(`${API_URL}auth/send-sms/`, { numbers: phone })
             .then((response) => {
                 console.log(response.data);
-                
+
                 if (response.data.return === true) {
                     navigate('/otp-verification', { state: { phone: phone, code: response.data.code } });
                 }
                 else {
-                    alert("Invalid Phone Number");
+                    setShowError(true);
+                    setSubmitting(false);
                 }
             })
             .catch((error) => {
                 console.error(error);
-                alert("OTP not sent");
+               setShowError(true);
+                setSubmitting(false);
             });
     };
 
 
 
     return (
-        <div style={{
-            alignItems: 'center', justifyContent: 'center', minHeight: '100px',
-            width: '300px', marginLeft: 'auto', marginRight: 'auto', marginTop: '10px'
+        <Container size="30rem" px={0}>
+            <Group position='apart'>
+                <div>
+                    <Text fz={30} fw="bolder" size="xl" align="left" mt="xl">
+                        Flat off 50% + 
+                    </Text>
+                    <Text fz={30} fw="bolder" size="lg" align="left" mt="xl">
+                        Free Shipping
+                    </Text>
+                    <Text  size="lg" align="left" mt="sm">
+                        On Your First Order
+                    </Text>
+                </div>
+                <img src="/img/jomo.jpg" alt="logo" style={{ width: '40%', height: '60%' }} />
 
-        }}>
-            <Card shadow="sm" padding="xl" style={{ Width: 2000 }}>
-                <h2 style={{
-                    textAlign: 'center', fontWeight: 'bolder',
-                    fontSize: '1.5rem', fontFamily: 'Greycliff CF, sans-serif',
-                    marginTop: '10px'
-                }}>Login or Sign UP</h2>
+
+            </Group>
+
+            <Card shadow="sm" p="xl" style={{ height: '50vh' }}>
+                <Group position="left" mt="xl">
+                    <Text fz={30} fw="bolder" size="xl" align="left" mt="xl">
+                        Login
+                    </Text>
+                    <Text fz={30} size="lg" align="left" mt="xl">
+                        or
+                    </Text>
+                    <Text fz={30} fw="bolder" size="xl" align="left" mt="xl">
+                        SignUp
+                    </Text>
+
+                </Group>
+                {
+                    submitting && <Loader size="xl" variant="dots"
+                        style={{ marginLeft: 'auto', marginRight: 'auto', marginTop: '10px' }}
+                    />
+                }
                 <form onSubmit={handleSubmit}>
                     <TextInput
                         mt="xl"
                         mb="xl"
-                        label="Phone Number"
                         size='lg'
                         value={phone}
                         onChange={(event) => setPhone(event.currentTarget.value)}
-                        placeholder="Enter your phone number"
+                        placeholder="Mobile Number"
                         required
+                        icon={<Text size="xl" color='black' > +91 | </Text>}
                     />
-                
+                    {
+                        showError && (
+                            <Text size="lg" align="left" mt="xl" color="red">
+                                Invalid Phone Number
+                            </Text>
+                        )
+                    }
+
+                    <Text size="lg" align="left" mt="xl">
+                        By Continuing, you agree to the
+                        <Link to="/terms-of-service" style={{ textDecoration: "none", color: '#ff3e6c' }}>
+                            {" "} Terms of Service
+                        </Link>
+                        {" "} &
+                        <Link to="/privacy-policy" style={{ textDecoration: "none", color: '#ff3e6c' }}>
+                            {" "} Privacy Policy
+                        </Link>
+                    </Text>
+                  
+
                     <button
                         className='btn btn-primary btn-block'
                         style={{
                             marginTop: 20,
-                            backgroundColor: '#1a202c',
+                            backgroundColor: '#ff3e6c',
                             marginLeft: 'auto',
                             marginRight: 'auto',
+                            width: '100%',
+                            borderRadius: 0,
+                            borderColor: '#ff3e6c',
                         }}
                         type="submit"
+                        disabled={submitting}
 
                     >
-                        Get Code
+                        Continue
                     </button>
                 </form>
+                <Text mb="xl" size="lg" align="left" mt="xl">
+                    Have Trouble Logging In? <Link to="/help" style={{ textDecoration: "none", color: '#ff3e6c' }}>
+                        Get Help
+                    </Link>
+
+                </Text>
 
             </Card>
-        </div>
 
-        // <Grid mx={10}>
-        //     <Grid.Col md={5} sm={12} mt={20}>
-        //         <Link to="/" style={{ textDecoration: "none" }}>
-        //             <Image src="/assets/images/logo.png" width={70} height={100} maw={240} mx="auto" radius="md" />
-        //         </Link>
+        </Container>
 
-        //         <Title
-        //             align="center"
-        //             sx={(theme) => ({ fontFamily: `Greycliff CF, ${theme.fontFamily}`, fontWeight: 900 })}
-        //         >
-        //             Welcome back!
-        //         </Title>
-        // <Text color="dimmed" size="sm" align="center" mt={5}>
-        //     Do not have an account yet?{' '}
-        //     <Anchor size="sm" component="button">
-        //         <Link to="/register" style={{ textDecoration: "none" }}>
-        //             Create account
-        //         </Link>
-        //     </Anchor>
-        // </Text>
 
-        //         <Paper withBorder shadow="md" p={30} mt={30} radius="md">
-        //             <Box component="form" onSubmit={handleSubmit}>
-        //                 <TextInput
-        //                     label="Email"
-        //                     placeholder="hello@mantine.dev"
-        //                     value={form.values.email}
-        //                     onChange={(event) => form.setFieldValue('email', event.currentTarget.value)}
-        //                     error={(form.errors.email && 'Invalid email') || (error && error)}
-        //                     radius="md"
-        //                     size="lg"
-        //                     withAsterisk
-        //                 />
-
-        //                 <PasswordInput
-
-        //                     label="Password"
-        //                     placeholder="Your password"
-        //                     value={form.values.password}
-        //                     onChange={(event) => form.setFieldValue('password', event.currentTarget.value)}
-        //                     error={error && error}
-        //                     radius="md"
-        //                     size="lg"
-        //                     withAsterisk
-        //                 />
-
-        //                 <Group position="apart" mt="lg">
-        //                     <Checkbox label="Remember me" />
-        //                     <Anchor component="button" size="sm">
-        //                         <Link to="forgot-password" style={{ textDecoration: "none" }}>
-        //                             Forgot password?
-        //                         </Link>
-
-        //                     </Anchor>
-        //                 </Group>
-        //                 <Button fullWidth mt="xl" type="submit" size="lg" loading={submit}>
-        //                     Sign in
-        //                 </Button>
-        //             </Box>
-        //         </Paper>
-        //     </Grid.Col>
-        //     <Grid.Col sm={7} lg={7}>
-        //         <Box mx="auto" >
-        //             <BackgroundImage
-        //                 src="https://images.unsplash.com/photo-1419242902214-272b3f66ee7a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80"
-        //                 radius="sm"
-        //                 style={{ height: "100vh" }}
-        //             >
-        //                 <Center p="md">
-        //                     <Text color="#fff">
-        //                         BackgroundImage component can be used to add any content on image. It is useful for hero headers and
-        //                         other similar sections
-        //                     </Text>
-        //                 </Center>
-        //             </BackgroundImage>
-        //         </Box>
-        //     </Grid.Col>
-        // </Grid>
 
     );
 }
